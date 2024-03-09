0c083910d13f19fd8e3a5f241660285d7cad0bb4
Vipin <vignesh.raju@daimler.com>
2024-03-09 21:14:52 +0530

    Qualifier

--- src/main/java/com/telusko/config/AppConfig.java
+++ src/main/java/com/telusko/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.telusko.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Scope;
 import com.telusko.Alien;
 import com.telusko.Computer;
 import com.telusko.Desktop;
+import com.telusko.Laptop;
 
 @Configuration
 public class AppConfig {
@@ -19,10 +21,15 @@ public class AppConfig {
 	}
 	
 	@Bean(name = "alien")
-	public Alien alien(@Autowired Computer com) {
+	public Alien alien(@Qualifier("com") @Autowired Computer com) {
 		Alien obj = new Alien();
 		obj.setAge(25);
 		obj.setLap(com);
 		return obj;
+	}
+	
+	@Bean
+	public Laptop laptop() {
+		return new Laptop();
 	}
 }
